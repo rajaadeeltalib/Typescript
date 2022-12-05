857116b452a3986a83a99a6e589dceb5dfaa5f22
rajaadeeltalib <adeeltalib6270@gmail.com>
2022-12-05 10:02:26 +0500

    function which is returning sum from array

---
+++ ts01/index.js
@@ -0,0 +1,7 @@
+// function which is returning sum from array
+function getTotal(numbers) {
+    return numbers.reduce(function (acc, item) {
+        return acc + item;
+    }, 0);
+}
+console.log(getTotal([5, 8, 10]));
--- ts01/index.ts
+++ ts01/index.ts
@@ -1,1 +1,8 @@
-let num: number = 25;
+// function which is returning sum from array
+
+function getTotal(numbers){
+    return numbers.reduce((acc, item) => {
+    return acc + item;
+    }, 0)
+    }
+    console.log(getTotal([5, 8, 10]));
