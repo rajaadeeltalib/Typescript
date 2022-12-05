f9afbfadae4cc0771e6ff0f2eb0e946fe40290ff
rajaadeeltalib <adeeltalib6270@gmail.com>
2022-12-05 10:35:44 +0500

    final code

--- ts01/index.ts
+++ ts01/index.ts
@@ -1,7 +1,7 @@
 // function which is returning sum from array
 
-function getTotal(numbers){
-    return numbers.reduce((acc, item) => {
+function getTotal(numbers: number[]):number{
+    return numbers.reduce((acc:number, item:number) => {
     return acc + item;
     }, 0)
     }
